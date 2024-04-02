51cc6c91750d282a2167a0d2fc73aebcdaf9f2c5
Gorthdzx <krislaksana123@gmail.com>
2024-04-02 09:21:13 +0700

    feat : button  intent to another activity

--- app/src/main/java/app/dito/sadhar_bio/activity/MainMenuActivity.kt
+++ app/src/main/java/app/dito/sadhar_bio/activity/MainMenuActivity.kt
@@ -6,43 +6,86 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import app.dito.sadhar_bio.MainActivity
-import app.dito.sadhar_bio.ui.theme.Sadhar_bioTheme
 
 class MainMenuActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Sadhar_bioTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    RedirectToHome(appComponent = this)
-                }
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                RedirectToHome(appComponent = this, modifier = Modifier)
             }
         }
     }
 }
 
-
 @Composable
-fun RedirectToHome(appComponent: ComponentActivity) {
+fun RedirectToHome(appComponent: ComponentActivity, modifier: Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = {
-            Intent(appComponent.applicationContext, MainActivity::class.java).apply {
-                appComponent.startActivity(this)
-            }
-        }) {
+        Spacer(modifier = modifier.size(550.dp))
+        Button(
+            onClick = {
+
+            },
+            modifier = modifier
+                .size(width = 335.dp, height = 40.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFC27B46),
+            )
+        ) {
+            Text(
+                text = "Sejarah Universitas Sanata Dharma",
+                fontSize = 16.sp,
+                fontWeight = FontWeight(400),
+                color = Color(0xFFFFFFFF),
+            )
+        }
+        Spacer(modifier = modifier.size(10.dp))
+        Button(
+            onClick = {
+
+            },
+            modifier = modifier
+                .size(width = 335.dp, height = 40.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFEC6400),
+            )
+        ) {
+            Text(text = "Daftar rektor Universitas Sanata Dharma")
+        }
+        Spacer(modifier = modifier.size(10.dp))
+        Button(
+            onClick = {
+                Intent(appComponent.applicationContext, MainActivity::class.java).apply {
+                    appComponent.startActivity(this)
+                }
+            },
+            modifier = modifier
+                .size(width = 335.dp, height = 40.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF874412),
+            )
+        ) {
             Text(text = "Home")
         }
     }
