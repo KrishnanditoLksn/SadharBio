9f23a2b49a869a4dce6baf3c260e9b0da9ddbd4b
Gorthdzx <krislaksana123@gmail.com>
2024-04-19 13:50:30 +0700

    refactor : resized logo in home screen

--- app/src/main/java/app/dito/sadhar_bio/MainActivity.kt
+++ app/src/main/java/app/dito/sadhar_bio/MainActivity.kt
@@ -6,9 +6,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,20 +20,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import app.dito.sadhar_bio.activity.MainMenuActivity
-import app.dito.sadhar_bio.ui.theme.Sadhar_bioTheme
+import app.dito.sadhar_bio.components.image_drawable.CustomImages
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Sadhar_bioTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    SabioApp(appComponent = this)
-                }
+            // A surface container using the 'background' color from the theme
+            Surface(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                SabioApp(appComponent = this)
             }
         }
     }
@@ -39,10 +41,18 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun SabioApp(appComponent: ComponentActivity) {
     Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        Spacer(modifier = Modifier.size(100.dp))
+        CustomImages(
+            imageId = R.drawable.usd,
+            imageDescription = "usd_gambar",
+            modifier = Modifier
+                .size(150.dp)
+                .width(200.dp)
+                .height(200.dp)
+        )
+        Spacer(modifier = Modifier.size(150.dp))
         ElevatedButton(
             onClick = {
                 Intent(appComponent.applicationContext, MainMenuActivity::class.java).apply {
@@ -53,8 +63,7 @@ fun SabioApp(appComponent: ComponentActivity) {
             )
         ) {
             Text(
-                text = "Next",
-                color = Color.White
+                text = "Next", color = Color.White
             )
         }
     }
@@ -63,5 +72,5 @@ fun SabioApp(appComponent: ComponentActivity) {
 @Preview(showBackground = true)
 @Composable
 fun SabioAppPreview() {
-
+    
 }
