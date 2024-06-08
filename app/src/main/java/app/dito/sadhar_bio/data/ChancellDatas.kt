c205de6af9a10d799c4c8fcc7160aec29059c3f6
Gorthdzx <krislaksana123@gmail.com>
2024-06-08 21:55:52 +0700

    feat : list of object

---
+++ app/src/main/java/app/dito/sadhar_bio/data/ChancellDatas.kt
@@ -0,0 +1,10 @@
+package app.dito.sadhar_bio.data
+
+import app.dito.sadhar_bio.Model.Chancellor
+
+class ChancelListDatas {
+    val callis = listOf(
+        Chancellor(name = "Pak eka", description = "Dosen staprob"),
+        Chancellor(name = "Pak antok", description = "Dosen Jarkom")
+    )
+}
